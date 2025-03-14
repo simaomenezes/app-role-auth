b09ba2a5a0fc3b226953da6094e0e8ef012edb4e
Simão Neto <simaomenezesneto@gmail.com>
2025-03-14 22:47:29 +0000

    chore: Make some settings to our app

---
+++ src/main/java/github/com/simaomenezes/roleapp/AppRoleAuthApplication.java
@@ -0,0 +1,13 @@
+package github.com.simaomenezes.roleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRoleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRoleAuthApplication.class, args);
+	}
+
+}
