a99a4b6a5b983c5083c1f9d448d930b44afd17b6
Simão Neto <simaomenezesneto@gmail.com>
2025-04-04 22:31:16 +0100

    chore: Change setting about all test

--- src/test/java/github/com/simaomenezes/roleapp/infrastructure/integrationtests/AbstractIntegrationTest.java
+++ src/test/java/github/com/simaomenezes/roleapp/infrastructure/integrationtests/AbstractIntegrationTest.java
@@ -4,16 +4,28 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Testcontainers
 public class AbstractIntegrationTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Container
+    @ServiceConnection
     static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:15");
 
     @BeforeAll
