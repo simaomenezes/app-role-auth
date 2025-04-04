488c24ae08973f9d336b9bf46b628301ec8e7d8d
Simão Neto <simaomenezesneto@gmail.com>
2025-04-04 22:28:36 +0100

    refact: Change table name

--- src/main/java/github/com/simaomenezes/roleapp/domain/entity/RoleEntity.java
+++ src/main/java/github/com/simaomenezes/roleapp/domain/entity/RoleEntity.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 import java.util.Set;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "role")
 public class RoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
